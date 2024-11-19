e590b62eb6e23d7dacee9fcedfd1648d9b488648
lyxal <36217120+lyxal@users.noreply.github.com>
2024-11-20 00:37:25 +1100

    People hate this one weird trick

--- lib/get-python-39.bash
+++ lib/get-python-39.bash
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-curl https://pyenv.run | bash
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-echo 'eval "$(pyenv init -)"' >> ~/.profile
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
-exec "$SHELL"
+asdf plugin-add python
+asdf install python 3.9.0
