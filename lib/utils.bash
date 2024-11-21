ad9b2001359d287e33118a0bd5248f87ec891500
lyxal <36217120+lyxal@users.noreply.github.com>
2024-11-21 18:52:57 +1100

    Local focal

--- lib/utils.bash
+++ lib/utils.bash
@@ -61,7 +61,7 @@ install_version() {
         # Ensure Python 3.9 is fully installed via asdf
         asdf plugin-add python || true
         asdf install python 3.9.0 || true
-        
+        asdf local python 3.9.0
 
         PYTHON_CMD=$(asdf which python)
         "$PYTHON_CMD" -m venv vynv --copies
