49ae24d32a430b088c9d0f180f69ee9681440f04
lyxal <36217120+lyxal@users.noreply.github.com>
2024-12-02 21:38:15 +1100

    Update utils.bash

--- lib/utils.bash
+++ lib/utils.bash
@@ -59,7 +59,7 @@ install_version() {
 	      	cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 	      	cd "$install_path"
 
-        unzip vyxal-linux.zip -d
+        unzip vyxal-linux.zip -d ..
 	       ls .
 	
       
